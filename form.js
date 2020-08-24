5eba9292cb6252e0a1f3bc13f7b9613d83d96472
yashasvijhawar <66315222+yashasvijhawar@users.noreply.github.com>
2020-08-24 16:57:09 +0530

    Add files via upload

---
+++ form.js
@@ -0,0 +1,27 @@
+class Form{
+    constructor(){
+
+    }
+
+    display(){
+      var title = createElement('h2');
+      title.html("car racing");
+      title.position(130,0);
+      var input = createInput("Name");
+      var button = createButton('play');
+      var greetings = createElement('h3');
+      input.position(130,160);
+      button.position(250,200);
+      button.mousePressed(function(){
+          input.hide();
+          button.hide();
+          var name = input.value()
+          playerCount+=1;
+          player.update(name);
+          player.updateCount(playerCount);
+          greetings.html("welcome"+name);
+          greetings.position(130,160);
+        })
+    }
+
+}
